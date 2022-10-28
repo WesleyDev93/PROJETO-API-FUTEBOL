bf9ef9e680f21b40c4b3f1ff5bbf9f1c19886788
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-10-28 15:53:11 -0300

    testes de obtenção e dados

--- front-end/assets/js/controller/mostraDados.js
+++ front-end/assets/js/controller/mostraDados.js
@@ -2,20 +2,20 @@ import { conectaApi } from "./controller.js";
 
 const lista = document.querySelector(".tabelaDados");
 
-function constroiJogadores(nome, gols) {
+function constroiJogadores(jogador, gols) {
     const jogadores = document.createElement("tr");
     jogadores.className = "tabelaDados";
     jogadores.innerHTML = `<tr>
-    <td>${nome}</td>
+    <td>${jogador}</td>
     <td>${gols}</td>
   </tr>`
 
     return jogadores;
 }
 
-async function listaJogador(elemento) {
+async function listaJogador() {
     const listaApi = await conectaApi.listaJogadores();
-    listaApi.forEach(elemento => lista.appendChild( constroiJogadores(elemento.nome, elemento.gols,)))
+    listaApi.forEach(elemento => lista.appendChild( constroiJogadores(elemento.jogador, elemento.gols,)))
 }
 
 listaJogador();
