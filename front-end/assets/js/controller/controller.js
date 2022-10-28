80622400a2a4fb81bf6b20f36ff1f3d0cd4d42b0
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-10-28 15:16:47 -0300

    configurando API

---
+++ front-end/assets/js/controller/controller.js
@@ -0,0 +1,19 @@
+async function listaVideos() {
+
+
+    const conexao = await fetch("http://localhost:3000/jogadores");
+    const conexaoConvertida = await conexao.json();
+
+    console.log (conexaoConvertida)
+
+    //return conexaoConvertida;
+}
+
+
+
+
+//export const conectaApi = {
+
+  //  listaVideos
+    
+//}
