11ac3decf1dec2df78a075288bccef0f003312fc
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-11-03 14:32:42 -0300

    correção de bugs de texto e de conexão com API

--- front-end/assets/js/controller/novoJogador.js
+++ front-end/assets/js/controller/novoJogador.js
@@ -5,16 +5,17 @@ const formulario = document.querySelector(".formulario")
 
 
 
-async function criarJogador(evento) {
+async function cadastroJogador(evento) {
     evento.preventDefault()
+
 
     const Jogador = document.querySelector(".Jogador").value
     const gols= document.querySelector(".gols").value
    
 
-    await conectaApi.criaJogagor(Jogador, gols);
+    await conectaApi.criaJogador(Jogador, gols);
     window.location.href = "index.html";
 }
 
-formulario.addEventListener("submit", evento => criarJogador(evento))
+formulario.addEventListener("submit", evento => cadastroJogador(evento))
 
